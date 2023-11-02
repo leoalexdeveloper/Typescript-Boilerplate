3c53b2ce36387f2675f98d84b09531f4cf147a1a
Leonardo <leoalexdeveloper@gmail.com>
2023-11-02 14:17:34 -0700

    chore: created Router type file in types folder

---
+++ src/Types/routerTypes.ts
@@ -0,0 +1,18 @@
+import type { Request, Response, NextFunction } from 'express'
+
+import type { ValidationChain } from 'express-validator'
+
+type supportedHtmlVerbs = 'get' | 'post' | 'put' | 'patch' | 'delete'
+
+type middlewareType = (req: Request, res: Response, next: NextFunction) => Promise<void> | void
+type actionType = (req: Request, res: Response) => Promise<void> | void
+
+interface routeType {
+  path: string
+  method: supportedHtmlVerbs
+  validation: ValidationChain[]
+  middlewares: middlewareType[]
+  actions: actionType[]
+}
+
+export type { Request, Response, NextFunction, ValidationChain, middlewareType, actionType, supportedHtmlVerbs, routeType }
--- src/Types/types.ts
+++
@@ -1,2 +0,0 @@
-export type { Request, Response, NextFunction } from 'express'
-export type { ValidationChain } from 'express-validator'
